dd918d609e9a1dc86a34120d1fdcb27c6b2d699c
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-08-11 12:24:30 +0300

    do task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const listEl = document.querySelector('#ingredients');
+
+function createEl(ingredients) {
+  const arr = []
+  ingredients.forEach(element => {
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.textContent = element
+    arr.push(ingredientsEl)
+  });
+
+  return listEl.prepend(...arr)
+}
+
+createEl(ingredients)
